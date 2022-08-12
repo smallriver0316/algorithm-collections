734ecdeb5ce304215eee58514eb540d5c3caad06
smallriver0316 <stream0316@gmail.com>
2022-08-12 23:12:27 +0900

    Modified code

--- Recursion/recursive_multiplication1.cpp
+++ Recursion/recursive_multiplication1.cpp
@@ -25,41 +25,45 @@ vector<int> calcExps(int x, int exp, vector<int> exps)
   }
 }
 
+int calcProduct(int x, int y)
+{
+  if (x == 0 || y == 0)
+  {
+    return 0;
+  }
+
+  int smaller = x >= y ? y : x;
+  int bigger = x > y ? x : y;
+
+  // maximum exponent
+  int maxExp = 0;
+  int shifted = 1 << maxExp;
+  while (smaller >= shifted)
+  {
+    shifted = 1 << ++maxExp;
+  }
+  maxExp -= 1;
+
+  vector<int> exps;
+  exps = calcExps(smaller, maxExp, exps);
+
+  int ret = 0;
+  for (vector<int>::iterator it = exps.begin(); it != exps.end(); it++)
+  {
+    ret += (bigger << *it);
+  }
+
+  return ret;
+}
+
 int main()
 {
   int x, y;
 
   cin >> x >> y;
 
-  if (x == 0 || y == 0)
-  {
-    cout << "result: 0" << endl;
-  }
-  else
-  {
-    int smaller = x >= y ? y : x;
-    int bigger = x > y ? x : y;
-
-    // maximum exponent
-    int maxExp = 0;
-    int shifted = 1 << maxExp;
-    while (smaller >= shifted)
-    {
-      shifted = 1 << ++maxExp;
-    }
-    maxExp -= 1;
-
-    vector<int> exps;
-    exps = calcExps(smaller, maxExp, exps);
-
-    int ret = 0;
-    for (vector<int>::iterator it = exps.begin(); it != exps.end(); it++)
-    {
-      ret += (bigger << *it);
-    }
-
-    cout << "result: " << ret << endl;
-  }
+  int ret = calcProduct(x, y);
+  cout << "result: " << ret << endl;
 
   return 0;
 }
